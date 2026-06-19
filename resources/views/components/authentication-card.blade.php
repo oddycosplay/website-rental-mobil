e5a702dc784d7c956c9a57a2d05d52ef31033039
Siliwangi Dev <dev@siliwangirental.local>
2026-06-20 05:31:34 +0700

    chore: hapus file lama dari root setelah dipindah ke folder backend/

--- resources/views/components/authentication-card.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="glass-card p-8 sm:p-10 rounded-3xl w-full">
-    {{ $slot }}
-</div>
--- resources/views/errors/419.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Halaman Kadaluarsa'))
-@section('code', '419')
-@section('message', __('Halaman telah kadaluarsa. Silakan muat ulang dan coba kembali.'))
